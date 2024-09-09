a0380f29406448c01155f9d161235a1e90dffc7d
AjmainMahtabAlam <ajmainm6@gmail.com>
2024-09-10 05:28:41 +0600

    updated dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -48,7 +48,7 @@ def login(request):
 
        if user is not None:
            auth.login(request, user)
-           messages.success(request, 'You are now logges in')
+           messages.success(request, 'You are now logged in')
            return redirect('dashboard')
        else:
            messages.error(request, 'Invalid Credentials')
@@ -58,7 +58,10 @@ def login(request):
         return render(request, 'accounts/login.html')
 
 def logout(request):
-    return redirect(request, 'index')
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are now logged out')
+        return redirect('index')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
